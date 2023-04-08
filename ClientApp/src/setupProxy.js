b6500eeffafa55ab1384ed45becdd444417e5769
Kevin Giang <giangkvn@gmail.com>
2023-04-08 12:57:52 -0600

    allow api call to go through proxy

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -5,7 +5,8 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:3669';
 
 const context =  [
-  "/contentItem"
+  "/contentItem",
+  "/contentItem/categorySummaries"
 ];
 
 module.exports = function(app) {
