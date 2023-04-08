8b58fa905cbfe62a0d930bd1b4051a21df3cf07b
Kevin Giang <giangkvn@gmail.com>
2023-04-08 15:18:13 -0600

    convert Table component to use MUI

--- ClientApp/src/components/ContentItemTable.js
+++ ClientApp/src/components/ContentItemTable.js
@@ -1,28 +1,43 @@
 import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@mui/material';
+
 import { formatCentValues } from '../utils/money-formatter';
 
 const ContentItemTable = ({ category, totalValueCents, data }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>{category}</th>
-          <th>${formatCentValues(totalValueCents)}</th>
-          <th><div>icon section</div></th>
-        </tr>
-      </thead>
-      <tbody>
-        {data && data.map(
-          item => (
-            <tr key={`${item.category}-${item.name}`}>
-              <td>{item.name}</td>
-              <td>${formatCentValues(item.valueCents)}</td>
-              <td>delete icon</td>
-            </tr>
-          )
-        )}
-      </tbody>
-    </table>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="Insured items table">
+        <TableHead>
+          <TableRow>
+            <TableCell>{category}</TableCell>
+            <TableCell>${formatCentValues(totalValueCents)}</TableCell>
+            <TableCell>icon section</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data && data.map(
+            item => (
+              <TableRow
+                key={`${item.category}-${item.name}`}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell>{item.name}</TableCell>
+                <TableCell>${formatCentValues(item.valueCents)}</TableCell>
+                <TableCell>Delete icon</TableCell>
+              </TableRow>
+            )
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
